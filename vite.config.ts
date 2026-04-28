c03c0891b20f2986bd8aff20a54710d2fba7b1f9
Andreson <andresondev@gmail.com>
2026-04-28 23:22:48 +0100

    Try it

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  appType: "spa",
   base: "./",
   server: {
     host: "::",
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    middlewareMode: false,
   },
   plugins: [
     react(),
