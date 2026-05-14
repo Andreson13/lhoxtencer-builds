1f92885592aa80e3cbdb8724514545f2fe137789
Andreson <andresondev@gmail.com>
2026-05-14 08:29:50 +0100

    Try it again

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   appType: "spa",
-  base: "./",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
